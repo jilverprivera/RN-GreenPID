e96060f3f6aa9161230dd4a698551774a9e288cb
Jilver Pacheco <jilverrivera1@gmail.com>
2022-04-15 22:02:39 -0500

    style/application constants

--- src/constants/COLORS.js
+++ src/constants/COLORS.js
@@ -1,8 +1,14 @@
 export const COLORS = {
+  background: '#F2F9F5',
   white: '#FFFFFF',
-  white2: '#F6F6F6',
-  black: '#1F2633',
-  primary: '#20D489',
-  secondary: '#649BEC',
-  danger: '#FF3333',
+  lightGray: '#D5D5D5',
+  darkGray: '#5B616A',
+  black: '#01091E',
+  primary: '#137344',
+  secondary: '#49D49D',
+
+  auxiliar_1: '#4F7CAC',
+  auxiliar_2: '#A6D9F7',
+  auxiliar_3: '#BCCCE0',
+  auxiliar_4: '#ED217C',
 };
--- src/constants/SIZES.js
+++ src/constants/SIZES.js
@@ -1,25 +1,24 @@
+/* eslint-disable prettier/prettier */
 import {Dimensions} from 'react-native';
+
 const {width, height} = Dimensions.get('window');
+
 export const SIZES = {
-  // GLOBAL
-  base: 8,
-  font: 14,
-  radius: 12,
-  padding: 24,
-  padding2: 36,
-
-  // FONT-SIZES
-  largeTitle: 50,
-  h1: 30,
-  h2: 22,
-  h3: 16,
-  h4: 14,
-  body1: 30,
-  body2: 20,
-  body3: 16,
-  body4: 14,
-
-  // APP DIMESIONS
   width,
   height,
+
+  margin: 20,
+  padding: 15,
+  borders: 10,
+
+  heading1: 32,
+  heading2: 24,
+  heading3: 18,
+  heading4: 16,
+  heading5: 14,
+
+  body1: 18,
+  body2: 16,
+  body3: 14,
+  body4: 12,
 };
