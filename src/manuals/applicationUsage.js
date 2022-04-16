ef561ade63d22abe55fe8f1e090474118663c376
Jilver Pacheco <jilverrivera1@gmail.com>
2022-04-16 16:56:47 -0500

    docs/markdown folder for user manuals

---
+++ src/manuals/applicationUsage.js
@@ -0,0 +1,20 @@
+export const AppUseMarkdown = `
+# h1 Heading 8-)
+
+## h2 Heading 2
+
+### h3 Heading 3
+
+#### h4 Heading 4
+
+##### h5 Heading 5
+
+###### h6 Heading 6
+
+ 
+**This is some bold text!**
+ 
+This is normal text
+
+![Minion](https://octodex.github.com/images/minion.png)
+`;
