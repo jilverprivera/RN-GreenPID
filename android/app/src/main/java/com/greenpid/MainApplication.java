6e9b479e835de87d42e7dc3ceb017bbdd96a5bb0
Jilver Pacheco <jilverrivera1@gmail.com>
2022-08-03 15:16:17 -0500

    configuration to deploy apk

--- android/app/src/main/java/com/greenpid/MainApplication.java
+++ android/app/src/main/java/com/greenpid/MainApplication.java
@@ -1,9 +1,13 @@
 package com.greenpid;
 
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,7 +32,8 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          // packages.add(new SplashScreenReactPackage());
+          
           return packages;
         }
 
