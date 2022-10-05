d7161bec78ed2dac37bbb1777bce720c8ddcfc68
Jilver Pacheco <jilverrivera1@gmail.com>
2022-10-04 22:27:06 -0500

    removed files

--- src/constants/FONTS.js
+++
@@ -1,7 +0,0 @@
-export const FONTS = {
-  light: 'AnekTamil-Light',
-  regular: 'AnekTamil-Regular',
-  medium: 'AnekTamil-Medium',
-  semibold: 'AnekTamil-SemiBold',
-  bold: 'AnekTamil-Bold',
-};
--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-import {COLORS} from './COLORS';
-import {SIZES} from './SIZES';
-import {FONTS} from './FONTS';
-
-export {COLORS, SIZES, FONTS};
